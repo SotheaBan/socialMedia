db6cf5c1007a0243cd07a7080508ae868a4dbb12
SotheaBan <ban.sothea@squeeze-inc.asia>
2024-12-13 15:38:50 +0700

    Create CRUD User Post

---
+++ backend/Post/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    
+    # this is where user are able to working on listing and posting 
+    path('post/' , views.PostListCreateView.as_view()), 
+    #this we using for updated with speicifies id 
+    path('post/<int:id>/', views.PostDeleteUpdateView.as_view())
+]
--- backend/Post/views.py
+++ backend/Post/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import Post 
+from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
+from .serializer import PostSerializer,UserPostSerializer
 # Create your views here.
+
+class PostListCreateView(ListCreateAPIView): 
+    queryset = Post.objects.all() 
+    serializer_class = PostSerializer
+
+
+class PostDeleteUpdateView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserPostSerializer
+    queryset = Post.objects.all() 
+    lookup_field = 'id'
+
+
+
+
+
+
+
+
