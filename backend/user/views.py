efbfd85247fcffdf84fbcc0e01ffa573de879fd5
Bunlong Ly <bunlongly03@gmail.com>
2024-12-13 16:56:06 +0700

    update with the views for user

--- backend/user/views.py
+++ backend/user/views.py
@@ -15,9 +15,6 @@ from datetime import datetime
 import pytz
 
 
-# from datetime import timedelta
-
-
 class CustomTokenObtainPairView(APIView):
     permission_classes = [AllowAny]
 
@@ -26,14 +23,12 @@ class CustomTokenObtainPairView(APIView):
         email = request.data.get("email")
         password = request.data.get("password")
 
-        # Validate input data
         if not email or not password:
             return Response(
                 {"detail": "Email and password are required."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        # Try to find the user by email (not by username)
         User = get_user_model()
         try:
             user = User.objects.get(email=email)
@@ -43,7 +38,6 @@ class CustomTokenObtainPairView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        # Check if the password is correct
         if not user.check_password(password):
             return Response(
                 {"detail": "Invalid email or password."},
@@ -66,7 +60,7 @@ class CustomTokenObtainPairView(APIView):
             utc_exp_time = datetime.utcfromtimestamp(exp_timestamp).replace(
                 tzinfo=pytz.utc
             )
-            cambodia_tz = pytz.timezone("Asia/Phnom_Penh")  # Cambodia timezone
+            cambodia_tz = pytz.timezone("Asia/Phnom_Penh")
             local_exp_time = utc_exp_time.astimezone(cambodia_tz)
 
             # Convert to string format for response
@@ -79,7 +73,6 @@ class CustomTokenObtainPairView(APIView):
             exp_time = None
             time_remaining = None
 
-        # Prepare the custom response with user data and tokens
         custom_response_data = {
             "access_token": access_token,
             "refresh_token": str(refresh),
@@ -101,14 +94,13 @@ class RegisterUser(APIView):
 
     def post(self, request):
         User = get_user_model()
-        # Check if the username already exists
+
         if User.objects.filter(username=request.data.get("username")).exists():
             return Response(
                 {"detail": "Username already taken."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        # Check if the email already exists
         if User.objects.filter(email=request.data.get("email")).exists():
             return Response(
                 {"detail": "Email is already registered."},
