e15c471ff8deaf79f3100d4d1d8cea6e54e129f1
SoPonloe <sovann.soponloe@squeeze-inc.asia>
2024-12-16 13:57:08 +0700

    made create notification view

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NotificationListView, MarkNotificationAsReadView
+from .views import NotificationListView, MarkNotificationAsReadView, CreateNotificationView
 
 urlpatterns = [
     path('', NotificationListView.as_view(), name='noti-list'),
-    path('<int:id>/read/', MarkNotificationAsReadView.as_view(), name='mark-noti-read')
+    path('<int:id>/read/', MarkNotificationAsReadView.as_view(), name='mark-noti-read'),
+    path('create/', CreateNotificationView.as_view, name='create-notification')
 ]
--- backend/notification/views.py
+++ backend/notification/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Notification
+from .models import User
 from .serializer import NotificationSerializer
+
 
 class NotificationListView(APIView):
     # permission_classes = [IsAuthenticated]  # Temporarily removed for testing
@@ -25,3 +28,27 @@ class MarkNotificationAsReadView(APIView):
         notification.is_read = True
         notification.save()
         return Response({"message": "Marked as Read"}, status=200)
+    
+class CreateNotificationView(APIView):
+
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        message = request.data.get('message')
+        notification_type = request.data.get('notification_type')
+
+        if not user_id or not message or not notification_type:
+            return Response({"error": "All Fields are required"})
+        
+        user = User.objects.filter(id=user_id).first()
+        if not user:
+            return Response({"error": "User not found"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        notification = Notification.objects.create(
+            user=user,
+            message=message,
+            notification_type=notification_type
+        )
+        
+        serializer = NotificationSerializer(notification)
+        return Response(serializer.data, status=201)
+
