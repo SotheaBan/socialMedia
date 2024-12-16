49393cb2617f5098fdfc7622eea0c58f76b92fa9
SotheaBan <ban.sothea@squeeze-inc.asia>
2024-12-16 16:16:34 +0700

    update url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Post.urls')),
     path('api/notification', include('notification.urls')),
-    path('api/', include('Search.urls'))
+    path('api/', include('Search.urls')),
     path('api/', include('comment.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
