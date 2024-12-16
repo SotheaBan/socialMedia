eb923951fc7d40a20baeb4f913c9c2a0f2dcd752
SoPonloe <sovann.soponloe@squeeze-inc.asia>
2024-12-16 15:03:43 +0700

    search for user works but the post dont yet

---
+++ backend/Search/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from user.models import User
+from Post.models import Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'profile_picture','bio']
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Post
+        feilds = ['id', 'author', 'content', 'image', 'created_at']
