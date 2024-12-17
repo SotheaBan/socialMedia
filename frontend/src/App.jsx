09538679e0a8a6b8bdd1f09323f2f944954e0e9e
khannchy-dg <choun.khannchy@squeeze-inc.asia>
2024-12-17 15:10:41 +0700

    ui registe and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css';
 import './index.css';
 import Homepage from './components/home/homePage'
+import LoginPage from './components//authentication/login/loginPage'
+import Register from './components/authentication/login/components/Register';
 function App() {
 
 
   return (
     <>
-     <Homepage/>
+      <Register />
     </>
   )
 }
