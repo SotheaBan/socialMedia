8b0827b7a911bb714e8024f0235d1388f7de2356
khannchy-dg <choun.khannchy@squeeze-inc.asia>
2024-12-18 15:31:32 +0700

    Update to date branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UserProfile from "./pages/Profile";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ProtectedRoute from "./components/ ProtectedRoute";
-import './App.css';
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import UserProfile from "./pages/Profile";
+//import Login from "./pages/Login";
+//import ProtectedRoute from "./components/ ProtectedRoute";
 import './index.css';
-import Homepage from './components/home/homePage'
-import LoginPage from './components//authentication/login/loginPage'
+//import Homepage from './components/home/homePage'
+//import LoginPage from './components//authentication/login/loginPage'
 import Register from './components/authentication/login/components/Register';
 
 function App() {
