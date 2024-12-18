7361a35470991360bd5583b3a72ed0d0b36e2bd4
SoPonloe <sovann.soponloe@squeeze-inc.asia>
2024-12-18 11:43:15 +0700

    Fix URL conflict by updating search path in Search app

--- backend/Search/urls.py
+++ backend/Search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import SearchView
 
 urlpatterns = [
+    # if  i put search/ it will not work because it overlap with urls.py in Post app
     path('searchh/', SearchView.as_view(), name='search')
 ]
