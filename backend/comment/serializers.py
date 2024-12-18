a21b3c260a99f8e17a99fe81f3726e866fb6abf3
khannchy-dg <choun.khannchy@squeeze-inc.asia>
2024-12-18 15:12:29 +0700

    Update .gitignore and clear cache

--- backend/comment/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Comment
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta :
-        model = Comment
-        fields = ['post','author', 'content','created_at','updated_at']
-
