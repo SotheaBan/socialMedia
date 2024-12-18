d488f389c0d30c23d990e0a8a17a675ddc5de213
Bunlong Ly <bunlongly03@gmail.com>
2024-12-18 10:13:30 +0700

    protectedRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserProfile from "./pages/Profile";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ ProtectedRoute";
 
 function App() {
   return (
@@ -11,8 +13,16 @@ function App() {
         <h1>Navbar</h1>
 
         <Routes>
-          <Route path="/profile" element={<UserProfile />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <UserProfile />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  <div>Regsiter Page</div>;
+};
+
+export default Register;
